bf6aebead08aeb73536a32caf391e0de89e27126
Guang.Gao <Guang.Gao@kaiostech.com>
2018-05-16 15:16:37 +0800

    run error

--- ipc/GmoduleChild.cpp
+++ ipc/GmoduleChild.cpp
@@ -1,9 +1,13 @@
 #include "GmoduleChild.h"
 
+#include <android/log.h>
+#define LOG_TAG "GmoduleChild"
+
 namespace mozilla {
 namespace dom {
 namespace gmodule {
 	GmoduleChild::GmoduleChild(){
+		__android_log_print(ANDROID_LOG_INFO, LOG_TAG, "constructor\n");
 		MOZ_COUNT_CTOR(GmoduleChild);
 	}
 
