15597b3e1e966bc529cd7501b2486d24a664f2af
Guang.Gao <Guang.Gao@kaiostech.com>
2018-05-16 19:48:30 +0800

    Add xpcom

---
+++ GmoduleManager.js
@@ -0,0 +1,29 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+"use strict";
+
+const DEBUG = true;
+function debug(s) { dump("-*- GmoduleManager: " + s + "\n"); }
+
+const Cc = Components.classes;
+const Ci = Components.interfaces;
+const Cu = Components.utils;
+
+Cu.import("resource://gre/modules/XPCOMUtils.jsm");
+Cu.import("resource://gre/modules/Services.jsm");
+
+function GmoduleManager() {};
+
+GmoduleManager.prototype = {
+	init: function() {
+		debug('init enter');
+	},
+
+	classID: Components.ID("{8ce7473f-b77d-4130-8b33-eaef78f14841}"),
+	contractID: "@mozilla.org/gmoduleManager;1",
+	QueryInterface: XPCOMUtils.generateQI([Ci.nsISupports,Ci.nsIGmoduleManager]),
+}
+
+this.NSGetFactory = XPCOMUtils.generateNSGetFactory([GmoduleManager]);
---
+++ gecko.dom.webild.Gmodule.webidl
@@ -0,0 +1,3 @@
+interface Gmodule : EventTarget {
+  long setTestData();
+};
